3ad315652c51b6d37dd31f0531a54dbaa4153130
Alex Buckley <alexmeb81@outlook.com>
2026-01-12 16:49:49 -0800

    Improved Problem base class structure and implemented DeJong implementation

--- include/Problem/Problem.h
+++ include/Problem/Problem.h
@@ -6,21 +6,26 @@
 // Base class for all optimization benchmark problems
 // Allows use of runtime polymorphism to evaluate different problems
 class Problem {
+protected:
+    const double lowerBound;  // Lower bound of input values
+    const double upperBound;  // Upper bound of input values
+    const char* const name;   // Function name
+
 public:
-    virtual ~Problem() = default;  // Always provide virtual destructor
+    // Constructor with member-initalizer list
+    Problem(double lb, double ub, const char* n)
+        : lowerBound(lb), upperBound(ub), name(n) {}
+
+    // Virtual destructor
+    virtual ~Problem() = default;
 
     // Evaluate a single solution vector x
     // Returns the scalar fitness value
     virtual double evaluate(const std::vector<double>& x) const = 0;
 
-    // Get lower bound for each variable
-    virtual double getLowerBound() const = 0;
-
-    // Get upper bound for each variable
-    virtual double getUpperBound() const = 0;
-
-    // Get name of the problem
-    virtual const char* getName() const = 0;
+    double getLowerBound() const { return lowerBound; }
+    double getUpperBound() const { return upperBound; }
+    const char* getName() const { return name; }
 };
 
 #endif // PROBLEM_H
