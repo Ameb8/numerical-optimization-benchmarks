3731f60d889cf38e5379184d01a0337017f46f93
Alex Buckley <alexmeb81@outlook.com>
2026-01-14 15:21:06 -0800

    Created concrete Problem implementations for DeJong 1 and Schwefel

---
+++ include/Problem/DeJongOne.h
@@ -0,0 +1,30 @@
+#ifndef DEJONG_H
+#define DEJONG_H
+
+#include "Problem.h"
+#include <vector>
+
+
+
+class DeJongOne : public Problem {
+private:
+    static constexpr double LOWER = -100.0;
+    static constexpr double UPPER = 100.0;
+    static constexpr std::string_view NAME = "DeJong_1";
+
+public:
+    // Constructor method
+    DeJongOne() : Problem(LOWER, UPPER, NAME) {}
+    
+    // Evaluate function
+    double evaluate(const std::vector<double>& x) const override {
+        double sum = 0.0;
+
+        for (double xi : x)
+            sum += xi * xi;
+        
+        return sum;
+    }
+};
+
+#endif
---
+++ include/Problem/Schwefel.h
@@ -0,0 +1,17 @@
+class Schwefel : public Problem {
+private:
+    static constexpr double LOWER = -512.0;
+    static constexpr double UPPER = 512.0;
+    static constexpr std::string_view NAME = "Schwefel";
+
+public:
+    Schwefel() : Problem(LOWER, UPPER, NAME.data()) {}
+
+    double evaluate(const std::vector<double>& x) const override {
+        double sum = 0.0;
+        for (double xi : x) {
+            sum += xi * std::sin(std::sqrt(std::abs(xi)));
+        }
+        return 418.9829 * x.size() - sum;
+    }
+};
