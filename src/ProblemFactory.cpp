1bd7c6bbad4ee14cca3b066c5113f9c4ea85ea9c
Alex Buckley <alexmeb81@outlook.com>
2026-01-14 23:05:07 -0800

    Added Rastrigin and Rosenbrock Problem implementations to ProblemFactory

--- src/ProblemFactory.cpp
+++ src/ProblemFactory.cpp
@@ -1,14 +1,14 @@
 #include "ProblemFactory.h"
-#include "Problem/Schwefel.h"   // Your concrete Problem implementation
-#include "Problem/DeJongOne.h"    // Your concrete Problem implementation
-//#include "Rosenbrock.h"
-//#include "Rastrigin.h"
-//#include "Griewangk.h"
-//#include "SineEnvelope.h"
-//#include "StretchedV.h"
-//#include "AckleyOne.h"
-//#include "AckleyTwo.h"
-//#include "EggHolder.h"
+#include "Problem/Schwefel.h"
+#include "Problem/DeJongOne.h"
+#include "Problem/Rosenbrock.h"
+#include "Problem/Rastrigin.h"
+//#include "Problem/Griewangk.h"
+//#include "Problem/SineEnvelope.h"
+//#include "Problem/StretchedV.h"
+//#include "Problem/AckleyOne.h"
+//#include "Problem/AckleyTwo.h"
+//#include "Problem/EggHolder.h"
 
 namespace {
 
@@ -24,10 +24,20 @@ std::unique_ptr<Problem> createDeJongOne() {
     return std::make_unique<DeJongOne>();
 }
 
+std::unique_ptr<Problem> createRosenbrock() {
+    return std::make_unique<Rosenbrock>();
+}
+
+std::unique_ptr<Problem> createRastrigin() {
+    return std::make_unique<Rastrigin>();
+}
+
 // Array mapping integer IDs -> factory functions
 constexpr std::array<Creator, 2> creators = {
-    &createSchwefel,   // ID 0
-    &createDeJongOne   // ID 1
+    &createSchwefel,    // ID 0
+    &createDeJongOne,   // ID 1
+    &createRosenbrock,  // ID 2
+    &createRastrigin  // ID 3
 
 };
 
