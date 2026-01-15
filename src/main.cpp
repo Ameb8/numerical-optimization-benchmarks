f60eb1dedcf8aed1bfa3c811622dba9088432a85
Alex Buckley <alexmeb81@outlook.com>
2026-01-14 19:21:53 -0800

    Updated main method to use Experiment class

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,9 @@
 #include <stdio.h>
-#include "mt.h"
+#include <iostream>
+#include "Experiment.h"
 
 
 int main() {
-    testTwister();
-
-    // Read and initialize config file
-
-    // Initialize RNG with config seed
-
-    // generate Problem object with ProblemFactory
-
-    // Create/initialize population
-
-    // Evaluate problem with given population
-
-    // Write to output file
-
-    // Cleanup memory
-
-    return 0;
+    Experiment exp;
+    return exp.runExperiment("config.toml");
 }
