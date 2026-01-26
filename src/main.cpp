2c743a2ce9155d6ffc1c2d271be4278c8e4f4321
Alex Buckley <alexmeb81@outlook.com>
2026-01-26 13:11:00 -0800

    Removed old code from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,14 @@
 #include "BenchmarkRunner.h"
 
 int main(int argc, char* argv[]) {
-    if(argc < 3) {
+    if(argc < 3) { // Read program arguments
         std::cerr <<"Error, not enough arguments provided\n";
         return 1;
     }
 
+    // Run benchmark
     BenchmarkRunner br;
     br.runBenchmarks(argv[1], argv[2]);
 
-    /*
-    std::string configFile = "config.toml";
-    
-    if(argc > 1)
-        configFile = argv[1];
-
-    TestRunner testRunner(configFile);
-    int numTests = testRunner.runTests();
-
-    std::cout << "\n" << numTests << " Experiments executed successfully\n";
-    */
     return 0;
 }
