0515b097e131d9b234ed1dfbdf327341731566aa
Alex Buckley <alexmeb81@outlook.com>
2026-02-02 02:21:53 -0800

    Implemented debug mode alert in main method

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string.h>
+
 #include "RunExperiments.h"
+#include "debug.h"
 
 int main(int argc, char* argv[]) {
+    // Debug print
+    debug::log("\nDebug Mode Enabled For:\t", argv[0]);
+
     if(argc < 3) { // Read program arguments
         std::cerr <<"Error, not enough arguments provided\n";
         return 1;
