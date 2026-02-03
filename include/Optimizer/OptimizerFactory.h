5b76774ab221d2eb629b33f4b1de7c643c10eca2
Alex Buckley <alexmeb81@outlook.com>
2026-02-03 10:13:55 -0800

    Small bug fix for Optimizer Factory optimizer generation

--- include/Optimizer/OptimizerFactory.h
+++ include/Optimizer/OptimizerFactory.h
@@ -14,9 +14,9 @@ public:
     static std::unique_ptr<Optimizer> initOptimizer(Problem& problem, ExperimentConfig& config, SolutionBuilder& builder) {
         if(config.optimizer == "blind")
             return std::make_unique<Blind>(builder, problem, config.maxIterations);
-        else if(config.optimizer == "local search")
+        else if(config.optimizer == "local")
             return std::make_unique<LocalSearch>(builder, problem, 1, config.neighborDelta, config.numNeighbors);
-        else if(config.optimizer == "repeated local search")
+        else if(config.optimizer == "repeated local")
             return std::make_unique<LocalSearch>(builder, problem, config.maxIterations, config.neighborDelta, config.numNeighbors);
         
         return nullptr;
